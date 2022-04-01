ada872cfbe440ecd302e8395244da56a99dc3574
fawazsullia <fawazsullia@gmail.com>
2022-04-01 15:59:52 +0400

    downloadable resume

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const staticRoutes = require("./routes/staticRoutes")
+const downloadRoutes =require("./routes/downloadRoutes")
 
 //middlewares
 app.use("/static" , express.static("public"))
 
 //route middlewares
 app.use("/", staticRoutes )
+app.use("/download", downloadRoutes)
 
 
 
