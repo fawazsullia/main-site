2f662e19f4561b9524e2771977d5e778072433c4
fawazsullia <fawazsullia@gmail.com>
2022-04-01 22:11:00 +0400

    chat api

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const {createServer} = require("http")
+const httpServer = createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(httpServer);
+
+const handleSocketChat = require("./utils/socketChat")
 
 const staticRoutes = require("./routes/staticRoutes")
 const downloadRoutes =require("./routes/downloadRoutes")
@@ -12,7 +18,11 @@ app.use("/static" , express.static("public"))
 app.use("/", staticRoutes )
 app.use("/download", downloadRoutes)
 
+// io.on("connection", (socket) => {
+//     console.log("connected")
+//     handleSocketChat(socket)
+// }) 
 
 
+httpServer.listen(PORT, ()=> console.log("Server started"))
 
-app.listen(PORT, ()=> console.log("Server started"))
