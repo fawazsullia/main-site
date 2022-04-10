cd5be9b84e14ba498dde3546c501d0e9f8191e7b
fawazsullia <fawazsullia@gmail.com>
2022-04-10 14:11:07 +0400

    testing express analytics

--- server.js
+++ server.js
@@ -7,22 +7,37 @@ const { Server } = require("socket.io");
 const io = new Server(httpServer);
 const {query} = require("./db/dbConfig")
 
+const xa = require("express-request-analytics")
+
 
 const staticRoutes = require("./routes/staticRoutes")
 const downloadRoutes =require("./routes/downloadRoutes");
 const apiRoutes = require("./routes/apiRoutes")
 const blogRoutes = require("./routes/blogRoutes")
 
-const {addRowQuery} = require("./db/chatQuery")
+const {addRowQuery} = require("./db/chatQuery");
 
 //middlewares
 app.use("/static" , express.static("public"))
+app.use(xa({cb :clbc}))
 
 //route middlewares
 app.use("/", staticRoutes )
 app.use("/download", downloadRoutes)
 app.use("/api", apiRoutes)
 app.use("/blog", blogRoutes)
+
+let temp = []
+
+
+function clbc(hit, deviceType, country, region, timeZone){
+temp.push({hit,deviceType,country,region, timeZone})
+}
+
+
+app.get("/test", (req,res)=>{
+  res.json(temp)
+})
 
 //handle socket connection
 io.on("connection", (socket)=>{
