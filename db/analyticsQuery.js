d45f6dfaeed45d50dfe6f40e5c49b3769dce511e
fawazsullia <fawazsullia@gmail.com>
2022-04-12 14:57:40 +0400

    query for analytics

--- db/analyticsQuery.js
+++ db/analyticsQuery.js
@@ -1,5 +1,11 @@
 const insertRow = `insert into analytics (hit, uniquehit, devicetype, country, timezone, region)
 values ($1,$2,$3,$4,$5,$6)`
-const getAnalytics = ""
+const getHighest = `select sum(hit) as hits, country, devicetype
+from analytics
+group by country 
+order by hits desc 
+limit 1;`
+const getTotal = `select sum(hit) as totalhits, sum(uniquehits) as totaluniquehits
+from analytics`
 
-module.exports = {insertRow}
+module.exports = {insertRow, getHighest, getTotal}
